d150b398a179de615c057b1311bd086a929bc645
Chriss78Vera <85882973+Chriss78Vera@users.noreply.github.com>
2021-06-14 14:34:41 +0000

    Al correo

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 #Numero de filas 
-
-
-
+print("escriban algo en el replit")
+print("no me sale lo de los commit jaajaja aiuda ")
+print("al correo ejeje")
+print("Buenas. Llegó e")
